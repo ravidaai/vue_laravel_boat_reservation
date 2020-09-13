3e0faa3e94d6a4f1005ec7ce43ab7ab887a49181
Ravi Kazi Karmacharya <ravidaai@gmail.com>
2020-09-13 09:14:26 +0545

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,23 @@
 <!-- Font-->
     <link rel="stylesheet" type="text/css" href="{{asset('css/opensans-font.css')}}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{asset('fonts/line-awesome/css/line-awesome.min.css')}}">
-<!-- Jquery -->
-<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('fonts/line-awesome/css/line-awesome.min.css')}}">
+
 <!-- Main Style Css -->
 <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
 
 @yield('header')
     </head>
-    <body class="form-v4">
+    <body class="form-v4 bg">
         <div class="container">
             <div class="header">
                 <h1 class="fast-easy">FAST & EASY - Boat Registration</h1>
                 <h2 class="subtitle">The only full service boat registration online</h2>
               </div>
             @yield('content')
-            <p style="text-align:center">
-                Made in America by Boat Registration USA LLC © 2020 All Rights Reserved
+            <p class="copyright-text">
+                Made in America by Boat Registration USA LLC © <?php echo date('Y'); ?> All Rights Reserved
               </p>
         </div>
     </body>
