4d909bf376c48333bce2bf11647913210a1cf590
Ravi Kazi Karmacharya <ravidaai@gmail.com>
2020-11-01 09:08:00 +0545

    fix

--- boat-nuxt/nuxt.config.js
+++ boat-nuxt/nuxt.config.js
@@ -39,6 +39,24 @@ olark.identify('5865-209-10-4526');
         type: 'text/javascript',
         charset: 'utf-8',
         async: true
+      },
+      {
+        hid:'hotjar',
+        innerHTML:`
+        <!-- Hotjar Tracking Code for https://boat-online-registration.com/ -->
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2071596,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+        `,
+        type: 'text/javascript',
+        charset: 'utf-8',
       }
     ],
   },
